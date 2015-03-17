0d66de86563d0a3f818cbb04855445ff07192ead
Ivan Sued <thefenry@gmail.com>
2015-03-17 14:57:26 -0700

    Created a model and a create controller
    The Review model will display the radio controls and will save the value
    appropriately.

---
+++ Models/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurveyPage.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int Expertise { get; set; }
+        public int Professionalism { get; set; }
+        public int Accountability { get; set; }
+    }
+}
