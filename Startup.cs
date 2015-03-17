8c772bcc476615233b62b4e4b2da98f909612486
Ivan Sued <thefenry@gmail.com>
2015-03-17 13:59:55 -0700

    Initial Commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SurveyPage.Startup))]
+namespace SurveyPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
