f566e7c28532eb08850c4dc36fdb8a7cf3b7bea8
Ivan Sued <thefenry@gmail.com>
2015-07-09 12:10:48 -0700

    Made some Modifications to the answers Model
    Aside from model changes i cleaned some code that was commented out
    earlier and fixed indentations.

--- SurveyPage/Models/Answer.cs
+++ SurveyPage/Models/Answer.cs
@@ -8,9 +8,10 @@ namespace SurveyPage.Models
     public class Answer
     {
         public int Id { get; set; }
-        public int Value { get; set; }
+        public int QuestionAnswer { get; set; }
         public int QuestionId { get; set; }
-
+        public int SurveyId { get; set; }
         public virtual Question Question { get; set; }
+        //public virtual Survey Survey { get; set; }
     }
 }
